221ae276d1c09b13a49248b5a67b271582b41d73
malinabochis <malina.bochis@bestcj.ro>
2026-03-29 16:43:41 +0300

    defined class in domain and wrote headers

--- domain/Transaction.cpp
+++ domain/Transaction.cpp
@@ -1,3 +1,85 @@
-//
-// Created by Asus on 29-Mar-26.
-//
+#include "Transaction.h"
+
+
+// default constructor
+Transaction::Transaction() {
+    id = -1;
+    sum = 0;
+    banknotes = nullptr;
+    noBanknotes = 0;
+}
+
+
+// constructor
+
+Transaction::Transaction(int id, int sum, PaymentBanknote* b, int noBanknotes) {
+    this->id = id;
+    this->sum = sum;
+    this->noBanknotes = noBanknotes;
+
+    banknotes = new PaymentBanknote[noBanknotes];
+    for (int i = 0; i < noBanknotes; i++)
+        banknotes[i] = b[i];
+}
+
+
+// copy constructor
+
+Transaction::Transaction(const Transaction &source) {
+    id = source.id;
+    sum = source.sum;
+    noBanknotes = source.noBanknotes;
+
+    banknotes = new PaymentBanknote[noBanknotes];
+    for (int i = 0; i < noBanknotes; i++)
+        banknotes[i] = source.banknotes[i];
+}
+
+
+// "=" operator
+
+Transaction &Transaction::operator=(const Transaction &source) {
+    if (this == &source)
+        return *this;
+
+    delete[] banknotes; // cand faci o atribuire catre o variabila, intai stergi legatura ei cu adresa spre care pointuia inainte
+
+    id = source.id;
+    sum = source.sum;
+    noBanknotes = source.noBanknotes;
+
+    banknotes = new PaymentBanknote[noBanknotes];
+    for (int i = 0; i < noBanknotes; i++)
+        banknotes[i] = source.banknotes[i];
+
+    return *this;
+}
+
+
+// "==" operator
+
+bool Transaction::operator==(const Transaction &source) const {
+    return id == source.id;
+}
+
+// getters
+int Transaction::getId() const {
+    return id;
+}
+
+int Transaction::getSum() const {
+    return sum;
+}
+
+PaymentBanknote *Transaction::getBanknotes() const {
+    return banknotes;
+}
+
+int Transaction::getNoBanknotes() const {
+    return noBanknotes;
+}
+
+// destructor:
+Transaction::~Transaction() {
+    delete[] banknotes;
+}
--- domain/Transaction.h
+++ domain/Transaction.h
@@ -1,16 +1,42 @@
 #ifndef SDA_CPP_TRANSACTION_H
 #define SDA_CPP_TRANSACTION_H
 
+struct PaymentBanknote {
+    int value;
+    int number;
+};
 
+class Transaction { // tranzactiile le pun intr-o multime later on..in lab 3
+private:
+    int id;
+    int sum;
+    PaymentBanknote* banknotes;  // tranzactia imi arata cat am pus, cat a iesit, cat a ramas
+    int noBanknotes;
 
+public:
+    Transaction(); // default constructor
 
+    // constructor
+    Transaction(int id, int sum, PaymentBanknote* b, int noBanknotes);
 
+    // copy constructor
+    Transaction(const Transaction& source); // fiecare tranzacție are "banii" ei proprii, chiar dacă sumele sunt identice.
 
+    // "=" operator
+    Transaction& operator=(const Transaction& source);
 
+    // "==" operator
+    bool operator==(const Transaction& source) const; // equal if id is equal
 
+    // getters
+    int getId() const;
+    int getSum() const;
+    PaymentBanknote* getBanknotes() const;
+    int getNoBanknotes() const;
 
-
-
+    // destructor:
+    ~Transaction();
+};
 
 
 #endif //SDA_CPP_TRANSACTION_H
